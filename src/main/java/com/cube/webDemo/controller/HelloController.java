de531750148a57d2b947a782220502f3d4448c09
cubecc <chancc_hk@yahoo.com.hk>
2020-05-21 11:16:10 +0800

    test

---
+++ src/main/java/com/cube/webDemo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.cube.webDemo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Hello World";
+	}
+
+}

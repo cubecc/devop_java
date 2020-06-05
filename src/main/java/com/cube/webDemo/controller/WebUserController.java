c83aa3cfafb92cd592b60d53b8f58b1335a55c31
cubecc <chancc_hk@yahoo.com.hk>
2020-06-05 18:18:34 +0800

    Test

--- src/main/java/com/cube/webDemo/controller/WebUserController.java
+++ src/main/java/com/cube/webDemo/controller/WebUserController.java
@@ -1,7 +1,10 @@
 package com.cube.webDemo.controller;
 
 import java.util.ArrayList;
+import java.util.Date;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
@@ -17,9 +20,12 @@ import com.cube.webDemo.dao.WebUserDao;
 import com.cube.webDemo.entity.WebUser;
 
 import antlr.collections.List;
+import lombok.extern.log4j.Log4j2;
 
 @Controller
 public class WebUserController {
+	private static final Logger logger = LogManager.getLogger(WebUserController.class);
+	
 	@Autowired
 	Environment env;
 	
@@ -28,6 +34,9 @@ public class WebUserController {
 		
 	@GetMapping("/login")
 	public String showLoginPage(Model model) {
+		Date currentDate = new Date();      
+		logger.info("Test Log from k8 : Curent Date and Time - " + currentDate.toString());		
+		
 		model.addAttribute("page_info", env.getProperty("env.name"));
 		//model.addAttribute("users", webUserDao.findAll());
 		//model.addAttribute("error", "invalid losssgin");
@@ -45,8 +54,7 @@ public class WebUserController {
 			model.addAttribute("error", "Invalid login");
 			return showLoginPage(model);
 		}
-		
-		
+				
 	}
 	
 	@RequestMapping("/getWebUserList")
